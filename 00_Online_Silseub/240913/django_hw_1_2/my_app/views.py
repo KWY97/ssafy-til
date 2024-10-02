107d952470838d2b929b768f4f22da5cadd1eb33
kwy97 <kwy1547@gmail.com>
2024-10-02 12:06:30 +0900

    241002

--- 00_Online_Silseub/240913/django_hw_1_2/my_app/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# view 함수의 첫 번째 인자가 항상 request인 이유는
-# view 함수를 호풀할 때, 첫번째 인자를 무조건  넣어주기 때문이다.
-# Create your views here.
-def hello(request):
-    return render(request, 'hello.html')
